2991be7b06b9f5e44bc6a9d5bc2d6da75cf4751c
nameerakhter <akhtarnameer@gmail.com>
2024-08-17 16:58:42 +0530

    web based wallet

---
+++ components/Navbar.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='bg-[#370617] h-[4rem] w-[27rem] rounded-full translate-x-[35rem] mt-4'>
+        <div className='flex items-center justify-center gap-[4rem] text-xl p-[1rem]'>
+        <a href="">Home</a>
+        <a href="">About</a>
+        <a href="">Contact</a>
+        </div>
+        
+    </div>
+  )
+}
+
+export default Navbar
